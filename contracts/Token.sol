9ef5202c2673dba137094177b6ae8a7c86c167ee
famouscat <onismaa@gmail.com>
2022-07-08 20:42:00 +0800

    update

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -6,6 +6,17 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract GLDToken is ERC20 {
     constructor() ERC20("Gold", "GLD") {
-        _mint(msg.sender, 1000);
+        _mint(msg.sender, 1000000000000000000000);
+    }
+
+    function _mintMinerReward() internal {
+        _mint(block.coinbase, 1000);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 value) internal virtual override {
+        if(!(from == address(0) && to == block.coinbase)) {
+            _mintMinerReward();
+        }
+        super._beforeTokenTransfer(from, to, value);
     }
 }
