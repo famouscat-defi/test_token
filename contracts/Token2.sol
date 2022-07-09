8feb19b49bfc69c18be2b49e291a0cb9fd597505
famouscat <onismaa@gmail.com>
2022-07-09 21:02:15 +0800

    update: totalSupply

--- contracts/Token2.sol
+++ contracts/Token2.sol
@@ -6,6 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token2 is ERC20 {
     constructor() ERC20("Token2", "T2") {
-        _mint(msg.sender, 2_000_000);
+        _mint(msg.sender, 2_000_000_000_000_000_000_000);
     }
 }
--- contracts/Token3.sol
+++ contracts/Token3.sol
@@ -6,6 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token3 is ERC20 {
     constructor() ERC20("Token3", "T3") {
-        _mint(msg.sender, 1_000_000_000);
+        _mint(msg.sender, 1_000_000_000_000_000_000_000_000_000);
     }
 }
